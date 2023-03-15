bb7b10f628dbe8a94c257d8c019cae7de56f5743
fredrikadr <fredrikadr@gmail.com>
2023-03-15 15:16:07 +0100

    add placeholder text and icons to articles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,13 +19,28 @@ export default function Home() {
       <Navigation />
       <div className='container'>
         <section id="about">
-          <article className='article' ></article>
+          <article className='about-me' >
+            <img alt="picture of me"></img>
+            <div className='text-container'>
+            <h1>Fredrik Andersen</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic magni ex vel iste tenetur magnam temporibus deserunt est cupiditate minima? Repudiandae, fugit accusantium. Optio excepturi iste perferendis recusandae id hic!</p>
+              <br></br>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae, adipisci iusto aliquid sequi, inventore ad quasi asperiores cupiditate nemo nihil tempora minima, animi eligendi dolorem est laboriosam recusandae iure?</p>
+            </div>
+          </article>
         </section>
         <section id="projects">
           <article className='article' id="projects"></article>
         </section>
         <section id="contact">
-          <article className='article' id="contact"></article>
+          <article className='contact-info' id="contact">
+            <a title="Connect with me on linkedin" href='https://www.linkedin.com/in/fredrik-andersen/'>
+            <Image alt="linkedin icon" width="200" height="200" src="/images/linkedin.png"></Image>
+            </a>
+            <a href="mailto:fredrikadr@gmail.com" title="Send me an e-mail">
+              <Image alt="email" width="200" height="200" src="/images/email.png"></Image>
+            </a>
+          </article>
 
         </section>
 
