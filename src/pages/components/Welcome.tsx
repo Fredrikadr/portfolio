a54841cb7f48475fd58eabae8549b7b0f0db90ef
fredrikadr <fredrikadr@gmail.com>
2023-03-15 13:40:21 +0100

    add text and animations to component

--- src/pages/components/Welcome.tsx
+++ src/pages/components/Welcome.tsx
@@ -1,9 +1,31 @@
 import { handleLinkClick } from "@/utils/utils";
+import { motion } from "framer-motion";
 
 export default function Welcome() {
+  const messages = ["Fredrik Andersen", "Full Stack Developer"];
   return (
-    <div className='welcome'>
-      <button onClick={() => handleLinkClick("about")}>Let's go!</button>
+    <div className="flex-container">
+
+      <div className='welcome'>
+        <motion.div className="text-container" animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+          {messages.map((message, index) => (
+            <motion.h1
+              key={index}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: index * 0.7, duration: 0.5 }}
+            >
+              {message}
+            </motion.h1>
+          ))}
+        </motion.div>
+        <motion.button
+          initial={{ opacity: 0, y: 500}}
+          animate={{ opacity: 1, y: 0}}
+          transition={{ delay: 1, duration: 0.7 }}
+          onClick={() => handleLinkClick("about")}>Let's go!</motion.button>
+      </div>
+
     </div>
   );
 }
