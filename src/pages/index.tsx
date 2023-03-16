701ba6359e109682bc91e0266ed8ece7300efb40
fredrikadr <fredrikadr@gmail.com>
2023-03-16 16:21:36 +0100

    added project card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,22 @@ import Welcome from './components/Welcome'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+let projects = [
+  {
+    title: "Quiz App",
+    description: "",
+    imagePath: "",
+    githubLink: "",
+    livePreview: "",
+    tech: [
+
+    ],
+
+  }
+]
+
+
 export default function Home() {
   return (
     <div>
@@ -17,7 +33,7 @@ export default function Home() {
 
       <Welcome />
       <Navigation />
-      <div className='container'>
+      <main className='container'>
         <section id="about">
           <article className='about-me' >
             <img alt="picture of me"></img>
@@ -30,21 +46,40 @@ export default function Home() {
           </article>
         </section>
         <section id="projects">
-          <article className='article' id="projects"></article>
+          <article className='projects'>
+            <h1>Projects</h1>
+            <div className='project-card'>
+              <h2>Quiz App</h2>
+              <figure>
+                <Image quality="100" width={500}
+                  height={500} alt="screenshot of quiz app project"  src="/images/quizapp.png"></Image>
+                <figcaption>
+                  <p>Uses the Open Trivia Database API to fetch questions based on user settings.</p>
+                </figcaption>
+              </figure>
+          </div>
+          </article>
         </section>
         <section id="contact">
-          <article className='contact-info' id="contact">
-            <a title="Connect with me on linkedin" href='https://www.linkedin.com/in/fredrik-andersen/'>
-            <Image alt="linkedin icon" width="200" height="200" src="/images/linkedin.png"></Image>
-            </a>
+          <article className='contact-info'>
+            <h1>Contact me</h1>
+            <div>
             <a href="mailto:fredrikadr@gmail.com" title="Send me an e-mail">
-              <Image alt="email" width="200" height="200" src="/images/email.png"></Image>
+              <Image alt="email" width="50" height="50" src="/images/email.png"></Image>
+              <span>Fredrikadr@gmail.com</span></a>
+            </div>
+            <div>
+            <a title="Connect with me on linkedin" href='https://www.linkedin.com/in/fredrik-andersen/'>
+              <Image alt="linkedin icon" width="50" height="50" src="/images/linkedin.png"></Image>
+              <span>Connect with me on LinkedIn</span>
             </a>
+
+            </div>
           </article>
 
         </section>
 
-      </div>
+      </main>
     </div>
 
 
