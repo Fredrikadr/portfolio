655da045ec7f35bf4245c9abd8170ae7a240110e
fredrikadr <fredrikadr@gmail.com>
2023-03-17 15:15:52 +0100

    add bio and projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,22 +10,6 @@ import Welcome from './components/Welcome'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-let projects = [
-  {
-    title: "Quiz App",
-    description: "",
-    imagePath: "",
-    githubLink: "",
-    livePreview: "",
-    tech: [
-
-    ],
-
-  }
-]
-
-
 export default function Home() {
   return (
     <div>
@@ -38,10 +22,12 @@ export default function Home() {
           <article className='about-me' >
             <img alt="picture of me"></img>
             <div className='text-container'>
-            <h1>Fredrik Andersen</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic magni ex vel iste tenetur magnam temporibus deserunt est cupiditate minima? Repudiandae, fugit accusantium. Optio excepturi iste perferendis recusandae id hic!</p>
+              <h1>Fredrik Andersen</h1>
+              <p>I'm full stack JavaScript developer with a focus on using the PERN stack. Although I'm new to the field, I have a strong desire to learn and apply new skills to real-world projects.</p>
               <br></br>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae, adipisci iusto aliquid sequi, inventore ad quasi asperiores cupiditate nemo nihil tempora minima, animi eligendi dolorem est laboriosam recusandae iure?</p>
+              <p>I'm always expanding my knowledge and skills through online courses, coding challenges, and personal projects. I'm highly motivated, collaborative, and always eager to work with others to find creative solutions to complex problems.</p>
+              <br></br>
+              <p>My ambition is to build a successful career in the tech industry, contributing to the development of innovative products and services that make a positive impact on society. I'm excited about the opportunities that lie ahead and am determined to achieve my goals through hard work, dedication, and a passion for learning.</p>
             </div>
           </article>
         </section>
@@ -49,30 +35,49 @@ export default function Home() {
           <article className='projects'>
             <h1>Projects</h1>
             <div className='project-card'>
-              <h2>Quiz App</h2>
+                <h2>Quiz App</h2>
               <figure>
+
                 <Image quality="100" width={500}
-                  height={500} alt="screenshot of quiz app project"  src="/images/quizapp.png"></Image>
+                  height={300} alt="screenshot of quiz app project" src="/images/quizapp.png"></Image>
                 <figcaption>
                   <p>Uses the Open Trivia Database API to fetch questions based on user settings.</p>
+                  <a target="blank" href="https://quiz-app-fvac.onrender.com/"><span>Live preview</span></a>
+                  <br></br>
+                  <a target="blank" href="https://github.com/Fredrikadr/quiz-app"><span>Code</span></a>
                 </figcaption>
               </figure>
-          </div>
+            </div>
+            <div className='project-card'>
+                <h2>Crazy Eights</h2>
+              <figure>
+                <Image quality="100" width={500}
+                  height={300} alt="screenshot of quiz app project" src="/images/crazyeights.png"></Image>
+                <figcaption>
+                  <p>Card game where you play versus the computer. Uses Deck of cards API to keep track of the deck.</p>
+                  <a target="blank" href="https://nextjs-crazy-eights.vercel.app/"><span>Live preview</span></a>
+                  <br></br>
+                  <a target="blank" href='https://github.com/Fredrikadr/nextjs-crazy-eights'><span>Code</span></a>
+                </figcaption>
+              </figure>
+
+            </div>
+
           </article>
         </section>
         <section id="contact">
           <article className='contact-info'>
             <h1>Contact me</h1>
             <div>
-            <a href="mailto:fredrikadr@gmail.com" title="Send me an e-mail">
-              <Image alt="email" width="50" height="50" src="/images/email.png"></Image>
-              <span>Fredrikadr@gmail.com</span></a>
+              <a href="mailto:fredrikadr@gmail.com" title="Send me an e-mail">
+                <Image alt="email" width="50" height="50" src="/images/email.png"></Image>
+                <span>Fredrikadr@gmail.com</span></a>
             </div>
             <div>
-            <a title="Connect with me on linkedin" href='https://www.linkedin.com/in/fredrik-andersen/'>
-              <Image alt="linkedin icon" width="50" height="50" src="/images/linkedin.png"></Image>
-              <span>Connect with me on LinkedIn</span>
-            </a>
+              <a title="Connect with me on linkedin" href='https://www.linkedin.com/in/fredrik-andersen/'>
+                <Image alt="linkedin icon" width="50" height="50" src="/images/linkedin.png"></Image>
+                <span>Connect with me on LinkedIn</span>
+              </a>
 
             </div>
           </article>
