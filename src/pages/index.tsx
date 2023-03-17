d167aae5d4572122cfcaa8b12136f0639abf1db7
fredrikadr <fredrikadr@gmail.com>
2023-03-17 15:26:42 +0100

    changed apostrophes in html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,9 +23,9 @@ export default function Home() {
             <Image src="" height={200} width={200} alt="picture of me"></Image>
             <div className='text-container'>
               <h1>Fredrik Andersen</h1>
-              <p>I'm full stack JavaScript developer with a focus on using the PERN stack. Although I'm new to the field, I have a strong desire to learn and apply new skills to real-world projects.</p>
+              <p>I&apos;m full stack JavaScript developer with a focus on using the PERN stack. Although I&apos;m new to the field, I have a strong desire to learn and apply new skills to real-world projects.</p>
               <br></br>
-              <p>I'm always expanding my knowledge and skills through online courses, coding challenges, and personal projects. I'm highly motivated, collaborative, and always eager to work with others to find creative solutions to complex problems.</p>
+              <p>I&apos;m always expanding my knowledge and skills through online courses, coding challenges, and personal projects. I'm highly motivated, collaborative, and always eager to work with others to find creative solutions to complex problems.</p>
               <br></br>
               <p>My ambition is to build a successful career in the tech industry, contributing to the development of innovative products and services that make a positive impact on society. I'm excited about the opportunities that lie ahead and am determined to achieve my goals through hard work, dedication, and a passion for learning.</p>
             </div>
